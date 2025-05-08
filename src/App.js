227c5e6c81096ae2296330cc0bc31073badc4a67
zoushuluan <130579770+zoushuluan@users.noreply.github.com>
2025-05-08 00:46:03 -0500

    App.js to modified to Madison, WI for CodePipeline

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Madison, WI! Modifed App.js -- Shuluan Zou</h1>
+      <h1>Hello Madison, WI! for CodePipeline -- Shuluan Zou</h1>
     </div>
   );
 }
